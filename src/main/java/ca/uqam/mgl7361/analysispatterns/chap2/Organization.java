88dfed16da28f3dad4596d82c76195b848dd8485
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2011-10-29 07:54:10 -0400

    some name to organization

--- src/main/java/ca/uqam/mgl7361/analysispatterns/chap2/Organization.java
+++ src/main/java/ca/uqam/mgl7361/analysispatterns/chap2/Organization.java
@@ -1,6 +1,12 @@
 package cq.uqam.mgl7361.analysispatterns.chap2;
 
 public class Organization extends Party {
+  private String name;
+
+  public Organization (String name) {
+    super();
+    this.name = name;
+  }
 }
 
 // vim:ts=2:sw=2
