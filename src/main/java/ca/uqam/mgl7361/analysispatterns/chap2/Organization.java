2d3e0a4eb4f836f58051c1c62935cdc30d8e770f
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2011-10-29 08:02:11 -0400

    Add hierarchy to organization

--- src/main/java/ca/uqam/mgl7361/analysispatterns/chap2/Organization.java
+++ src/main/java/ca/uqam/mgl7361/analysispatterns/chap2/Organization.java
@@ -2,10 +2,15 @@ package cq.uqam.mgl7361.analysispatterns.chap2;
 
 public class Organization extends Party {
   private String name;
+  private Organization parent;
 
   public Organization (String name) {
+    this(name, null);
+  }
+  public Organization (String name, Organization parent) {
     super();
     this.name = name;
+    this.parent = parent;
   }
 }
 
