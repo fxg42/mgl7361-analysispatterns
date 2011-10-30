ebe7e549c7c13a7d585a18b70b7caf93dd78242e
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2011-10-30 14:55:55 -0400

    Clean up Organization

--- src/main/java/ca/uqam/mgl7361/analysispatterns/chap2/Organization.java
+++ src/main/java/ca/uqam/mgl7361/analysispatterns/chap2/Organization.java
@@ -3,32 +3,8 @@ package cq.uqam.mgl7361.analysispatterns.chap2;
 import java.util.*;
 
 public class Organization extends Party {
-  private Organization parent;
-
   public Organization (String name, PartyType type) {
-    this(name, type, null);
-  }
-  public Organization (String name, PartyType type, Organization parent) {
     super(name, type);
-    setParent(parent);
-  }
-
-  public Organization getParent () {
-    return parent;
-  }
-  public void setParent (Organization parent) {
-    if (parent != null && parent.getAncestors().contains(this))
-      throw new IllegalArgumentException("cannot be an ancestor of self");
-    this.parent = parent;
-  }
-
-  protected Set<Organization> getAncestors () {
-    Set<Organization> ancestors = new HashSet<Organization>();
-    if (parent != null) {
-      ancestors.add(parent);
-      ancestors.addAll(parent.getAncestors());
-    }
-    return ancestors;
   }
 }
 
