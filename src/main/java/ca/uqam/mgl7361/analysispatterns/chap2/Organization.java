b1c6843f16cfc64353c8959898027497d52db347
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2011-10-30 20:27:56 -0400

    Clean up.

--- src/main/java/ca/uqam/mgl7361/analysispatterns/chap2/Organization.java
+++ src/main/java/ca/uqam/mgl7361/analysispatterns/chap2/Organization.java
@@ -1,7 +1,5 @@
 package cq.uqam.mgl7361.analysispatterns.chap2;
 
-import java.util.*;
-
 public class Organization extends Party {
   public Organization (String name, PartyType type) {
     super(name, type);
